a9ecf8d75f14da6cf3a09f98067c4b980996108c
乂ᴇᴏꋊ <125651298+xeon2account@users.noreply.github.com>
2025-09-23 23:10:57 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
-API_ID = 123456
-API_HASH = "your_api_hash"
-BOT_TOKEN = "your_bot_token"
+import os
+
+API_ID = int(os.getenv("API_ID"))
+API_HASH = os.getenv("API_HASH")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
