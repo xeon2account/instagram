e27f8ae2f241f9951994a763a53a4ab148905b5e
乂ᴇᴏꋊ <125651298+xeon2account@users.noreply.github.com>
2025-09-23 22:43:04 +0530

    Create download_handler.py

---
+++ handlers/download_handler.py
@@ -0,0 +1,23 @@
+from pyrogram import filters
+from utils.ig_api import fetch_ig_media
+
+async def download_ig(client, message):
+    url = message.text.strip()
+    
+    if "instagram.com" not in url:
+        await message.reply_text("Please send a valid Instagram URL.")
+        return
+    
+    data = fetch_ig_media(url)
+    
+    if data["status"] and data["data"]:
+        media = data["data"][0]
+        media_url = media["url"]
+        media_type = media["type"]
+        
+        if media_type == "video":
+            await message.reply_video(media_url, caption=f"Downloaded by {data['creator']}")
+        else:
+            await message.reply_text("Media type not supported yet.")
+    else:
+        await message.reply_text("Failed to download. Make sure the link is correct.")
