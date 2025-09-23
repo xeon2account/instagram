77bc6f9b6e2bda247f582f7de1fa155d9867d261
乂ᴇᴏꋊ <125651298+xeon2account@users.noreply.github.com>
2025-09-23 23:35:48 +0530

    Update ig_api.py

--- utils/ig_api.py
+++ utils/ig_api.py
@@ -1,6 +1,6 @@
 import requests
 
-def fetch_ig_media(url):
+def fetch_ig_media(url: str):
     api_url = f"https://api-aswin-sparky.koyeb.app/api/downloader/igdl?url={url}"
     response = requests.get(api_url)
     return response.json()
